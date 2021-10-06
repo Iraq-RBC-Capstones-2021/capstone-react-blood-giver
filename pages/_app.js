66bf2912733bcfa4e4af05ac03e7db0b48b21ede
ahmad <ahmad.00079821@gmail.com>
2021-10-06 22:34:37 +0300

    Install and setup Chakra UI Fixes #1

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,19 @@
 import { wrapper } from "../store";
 import "../styles/globals.css";
+//Font
+import "@fontsource/roboto";
+import "@fontsource/open-sans";
+//Chakra Provider
+import { ChakraProvider } from "@chakra-ui/react";
+//theme
+import { designTheme } from "../styles/theme";
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={designTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default wrapper.withRedux(App);
