03d52a58ea91d5c5f438daff65d6169d9fc625c1
Mahmoud Kassah <kassah.webdev@gmail.com>
2021-10-07 08:13:33 +0300

    setup(firebase): configure the app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { wrapper } from "../store";
 import "../styles/globals.css";
+import app from "../firebaseApp";
 
 const App = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
