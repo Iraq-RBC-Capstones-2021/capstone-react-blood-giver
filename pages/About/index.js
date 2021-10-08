bd662bff1e3c67acd8e00060633f5a0a8b771fdd
marwa <marwajawad19@gmail.com>
2021-10-08 08:45:58 +0300

    create the about us page

---
+++ pages/About/index.js
@@ -0,0 +1,10 @@
+import CustomPage from "../../components/CustomPage";
+function index() {
+  return (
+    <CustomPage>
+      <h1>About us page </h1>
+    </CustomPage>
+  );
+}
+
+export default index;
