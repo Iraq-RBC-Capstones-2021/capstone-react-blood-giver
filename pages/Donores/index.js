e805d9cdeb7b62fb7105d8d55c9a56ade58db058
marwa <marwajawad19@gmail.com>
2021-10-08 08:47:10 +0300

    create the donores page

---
+++ pages/Donores/index.js
@@ -0,0 +1,10 @@
+import CustomPage from "../../components/CustomPage";
+function index() {
+  return (
+    <CustomPage>
+      <h1>Donores page </h1>
+    </CustomPage>
+  );
+}
+
+export default index;
