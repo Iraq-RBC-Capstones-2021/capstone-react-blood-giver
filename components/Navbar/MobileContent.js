372c5173aea4364b1c884d7eeb18f0b577c2ac89
marwa <marwajawad19@gmail.com>
2021-10-08 08:41:28 +0300

    create MobileContent component

---
+++ components/Navbar/MobileContent.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { Flex, Button, IconButton } from "@chakra-ui/react";
+import NavItem from "../Navbar/NavItem";
+import { CloseIcon } from "@chakra-ui/icons";
+const Index = ({ display, changeDisplay }) => {
+  return (
+    <Flex
+      w="100vw"
+      display={display}
+      bgColor="#FF867F"
+      zIndex={20}
+      h="100vh"
+      pos="fixed"
+      top="0"
+      left="0"
+      overflowY="auto"
+      flexDir="column"
+    >
+      <Flex justify="flex-end">
+        <IconButton
+          mt={2}
+          mr={2}
+          aria-label="Open Menu"
+          size="lg"
+          icon={<CloseIcon />}
+          onClick={() => changeDisplay("none")}
+        />
+      </Flex>
+
+      <Flex flexDir="column" align="center">
+        <NavItem href="/" label="Home" />
+        <NavItem href="/About" label="About us" />
+        <NavItem href="/Donores" label="Donores" />
+        <NavItem href="/Contact" label="Contact us" />
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Index;
