ccc56efaf878b18468390ce9b3c8d28b6f6967b8
marwa <marwajawad19@gmail.com>
2021-10-08 10:05:14 +0300

    addng label and fix the space between the navItem

--- components/Navbar/NavItem.js
+++ components/Navbar/NavItem.js
@@ -6,9 +6,10 @@ function index({ href, label }) {
       <Button
         as="a"
         variant="ghost"
-        aria-label="Contact"
+        aria-label={label}
         color="primary"
-        my={5}
+        my="5"
+        mr="5"
         w="90%"
       >
         {label}
