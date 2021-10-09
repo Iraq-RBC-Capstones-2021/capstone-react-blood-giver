ea1495bfeda1890ae290f663e41ebad8d21a2f88
marwa <marwajawad19@gmail.com>
2021-10-09 21:18:26 +0300

    convert navbar items into an array of items

--- components/Navbar/NavItem.js
+++ components/Navbar/NavItem.js
@@ -1,20 +1,21 @@
 import NextLink from "next/link";
 import { Button } from "@chakra-ui/react";
-function index({ href, label }) {
-  return (
-    <NextLink href={href} passHref>
+const Pages = ["Home", "Contact", "Donores", "About"];
+function index() {
+  return Pages.map((item, i) => (
+    <NextLink href={item === "Home" ? "/" : `/${item}`} passHref>
       <Button
         as="a"
         variant="ghost"
-        aria-label={label}
+        aria-label={item}
         color="primary"
         my="5"
         mr="5"
         w="90%"
       >
-        {label}
+        {item}
       </Button>
     </NextLink>
-  );
+  ));
 }
 export default index;
