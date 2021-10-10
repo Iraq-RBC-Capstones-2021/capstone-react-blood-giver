84969b6fcb0e21cde5aa84b33225673a2e927465
marwa <marwajawad19@gmail.com>
2021-10-10 16:10:21 +0300

    adding unique key

--- components/Navbar/NavItem.js
+++ components/Navbar/NavItem.js
@@ -3,20 +3,22 @@ import { Button } from "@chakra-ui/react";
 const Pages = ["Home", "Contact", "Donores", "About"];
 function index() {
   return Pages.map((item, i) => (
-    <NextLink key={i} href={item === "Home" ? "/" : `/${item}`} passHref>
-      <Button
-        as="a"
-        variant="ghost"
-        aria-label={item}
-        color="primary"
-        my="5"
-        mr="5"
-        w="90%"
-        key={i}
-      >
-        {item}
-      </Button>
-    </NextLink>
+    <div key={item}>
+      <NextLink href={item === "Home" ? "/" : `/${item}`} passHref>
+        <Button
+          as="a"
+          variant="ghost"
+          aria-label={item}
+          color="primary"
+          my="5"
+          mr="5"
+          w="90%"
+          key={i}
+        >
+          {item}
+        </Button>
+      </NextLink>
+    </div>
   ));
 }
 export default index;
