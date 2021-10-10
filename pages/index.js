094623013a132ce0e986c192b348907420d38e4d
Mahmoud Kassah <kassah.webdev@gmail.com>
2021-10-10 13:07:54 +0300

    setup(localization): add custom hook for switch locale

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,10 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 import { FormattedMessage } from "react-intl";
+import useLocaleSwitch from "../i18n/useLocaleSwitch";
 
 export default function Home() {
+  const { switchLocale } = useLocaleSwitch();
   return (
     <div className={styles.container}>
       <Head>
@@ -18,6 +20,8 @@ export default function Home() {
           <FormattedMessage defaultMessage="Welcome at Blood Giver" />
         </h1>
         <FormattedMessage defaultMessage="one blood donation could saves up to three lives" />
+        <button onClick={() => switchLocale("ar")}>Ar</button>
+        <button onClick={() => switchLocale("en")}>En</button>
       </main>
     </div>
   );
