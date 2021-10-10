cd7aac9a1b9c6a94e6cb211e58d8d16f90d6b540
ElyasSalar <elyassalar8@gmail.com>
2021-10-10 14:57:52 +0300

    fix(import): remove importing unnecessary imports in pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,6 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
-// components
-import DonerCard from "../components/doner-card";
-
 export default function Home() {
   return (
     <div className={styles.container}>
