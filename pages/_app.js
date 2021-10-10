0746335d540ca5fcab467d480294488ab27ebcfb
marwa <marwajawad19@gmail.com>
2021-10-10 21:27:20 +0300

    createthe page layout and add footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,27 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Grid, GridItem, Container } from "@chakra-ui/react";
 import "@fontsource/roboto";
 import "@fontsource/open-sans";
 import { wrapper } from "../store";
 import "../styles/globals.css";
 import { designTheme } from "../styles/theme";
 import NavBar from "../components/Navbar";
+import Footer from "../components/Footer";
 const App = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={designTheme}>
-      <NavBar />
-      <Component {...pageProps} />
+      <Grid>
+        <GridItem colSpan={1}>
+          <NavBar />
+        </GridItem>
+        <GridItem>
+          <Container maxW="container.xl">
+            <Component {...pageProps} />
+          </Container>
+        </GridItem>
+        <GridItem position="fixed" bottom="0px" w="100%" colSpan={1}>
+          <Footer />
+        </GridItem>
+      </Grid>
     </ChakraProvider>
   );
 };
