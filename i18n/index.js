a3e4fd746cdcabe336fc2040c0272555c12a38c1
Mahmoud Kassah <kassah.webdev@gmail.com>
2021-10-12 07:36:39 +0300

    setup(localization): change dir when switch locale

--- i18n/index.js
+++ i18n/index.js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useRef } from "react";
 import English from "./compiled-locales/en.json";
 import Arabic from "./compiled-locales/ar.json";
 import { IntlProvider } from "react-intl";
@@ -8,6 +8,7 @@ const defaultLocale = process.env.DEFAULT_LOCAL || "en";
 
 const LocalizationProvider = ({ children }) => {
   const { locale = defaultLocale } = useRouter();
+  const ref = useRef();
 
   const messages = useMemo(() => {
     switch (locale) {
@@ -18,9 +19,20 @@ const LocalizationProvider = ({ children }) => {
     }
   }, [locale]);
 
+  useEffect(() => {
+    switch (locale) {
+      case "ar":
+        ref.current.dir = "rtl";
+        break;
+      default:
+        ref.current.dir = "ltr";
+        break;
+    }
+  }, [locale]);
+
   return (
     <IntlProvider locale={locale} messages={messages}>
-      {children}
+      <div ref={ref}>{children}</div>
     </IntlProvider>
   );
 };
