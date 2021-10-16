6bd5de4bf4d26c6a469f431f2d29ac27849de2c7
marwa <marwajawad19@gmail.com>
2021-10-16 19:50:24 +0300

    moving the home to the componentfolder

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import styles from "../styles/Home.module.css";
-import MainWidget from "./Home/MainWidget";
-import Statistics from "./Home/Statistics";
+import MainWidget from "../components/Home/MainWidget";
+import Statistics from "../components/Home/Statistics";
 import { Box } from "@chakra-ui/react";
-import Concerns from "./Home/Concerns";
+import Concerns from "../components/Home/Concerns";
 export default function Home() {
   return (
     <Box>
