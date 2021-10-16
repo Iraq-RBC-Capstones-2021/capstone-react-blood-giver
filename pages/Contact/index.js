caa82225459261191fc8108f1121e34c23031080
marwa <marwajawad19@gmail.com>
2021-10-16 20:37:25 +0300

    adding contact us page

--- pages/Contact/index.js
+++ pages/Contact/index.js
@@ -1,5 +1,51 @@
+import {
+  Text,
+  Container,
+  Stack,
+  Textarea,
+  Flex,
+  Input,
+  Button,
+  Spacer,
+} from "@chakra-ui/react";
+import { formatTime } from "@formatjs/intl";
+import { FormattedMessage } from "react-intl";
 function index() {
-  return <h1>Contact us page </h1>;
+  return (
+    <Container maxW="container.xl">
+      <Flex flexDirection="column" alignItems="center" justifyContent="center">
+        <Text color="light" fontSize="60px">
+          <FormattedMessage defaultMessage="Connect us" />
+        </Text>
+        <Text fontSize="19px" w="600px" align="center">
+          <FormattedMessage
+            defaultMessage=" If you want to get some help or just to ask us a question,use the form
+          below."
+          />
+        </Text>
+        <Stack m="20px" w="600px">
+          <Flex gridGap="20px">
+            <Input variant="flushed" placeholder="Name" borderColor="primary" />
+
+            <Input
+              variant="flushed"
+              placeholder="Email"
+              borderColor="primary"
+            />
+          </Flex>
+
+          <Textarea
+            variant="flushed"
+            placeholder="Message"
+            borderColor="primary"
+          />
+        </Stack>
+        <Button mb="2" bg="primary" color="white" _hover={{ color: "white" }}>
+          <FormattedMessage defaultMessage="Send" />
+        </Button>
+      </Flex>
+    </Container>
+  );
 }
 
 export default index;
