d820dd6d06037a81ca12329f94d47967af587de5
marwa <marwajawad19@gmail.com>
2021-10-18 02:20:33 +0300

    adding validation to the input

--- pages/Contact/index.js
+++ pages/Contact/index.js
@@ -1,51 +1,50 @@
-import {
-  Text,
-  Container,
-  Stack,
-  Textarea,
-  Flex,
-  Input,
-  Button,
-  Spacer,
-} from "@chakra-ui/react";
-import { formatTime } from "@formatjs/intl";
-import { FormattedMessage } from "react-intl";
-function index() {
+import { Text, Container, Flex } from "@chakra-ui/react";
+import { FormattedMessage, useIntl } from "react-intl";
+import { useFormik } from "formik";
+import validationSchema from "./validationScema";
+import FormInput from "./FormInput";
+function Index() {
+  const {
+    values,
+    errors,
+    touched,
+    setFieldValue,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+  } = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+    validationSchema: validationSchema,
+    validationOnBlur: true,
+    onSubmit: () => {},
+  });
+  const intl = useIntl();
   return (
     <Container maxW="container.xl">
       <Flex flexDirection="column" alignItems="center" justifyContent="center">
         <Text color="light" fontSize="60px">
           <FormattedMessage defaultMessage="Connect us" />
         </Text>
-        <Text fontSize="19px" w="600px" align="center">
+        <Text fontSize="17px" w="600px" mb="40px" align="center">
           <FormattedMessage
             defaultMessage=" If you want to get some help or just to ask us a question,use the form
           below."
           />
         </Text>
-        <Stack m="20px" w="600px">
-          <Flex gridGap="20px">
-            <Input variant="flushed" placeholder="Name" borderColor="primary" />
-
-            <Input
-              variant="flushed"
-              placeholder="Email"
-              borderColor="primary"
-            />
-          </Flex>
-
-          <Textarea
-            variant="flushed"
-            placeholder="Message"
-            borderColor="primary"
-          />
-        </Stack>
-        <Button mb="2" bg="primary" color="white" _hover={{ color: "white" }}>
-          <FormattedMessage defaultMessage="Send" />
-        </Button>
+        <FormInput
+          errors={errors}
+          values={values}
+          touched={touched}
+          handleBlur={handleBlur}
+          handleChange={handleChange}
+        />
       </Flex>
     </Container>
   );
 }
 
-export default index;
+export default Index;
