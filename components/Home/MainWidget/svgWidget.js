e7407cbad0965a74de02dddc81dc60bc184f7643
marwa <marwajawad19@gmail.com>
2021-10-17 16:15:55 +0300

    replace custome style with tailwind classes

--- components/Home/MainWidget/svgWidget.js
+++ components/Home/MainWidget/svgWidget.js
@@ -14,20 +14,20 @@ const index = () => {
     >
       <style type="text/css"></style>
       <g>
-        <g className="st1">
+        <g className="inline">
           <defs>
             <rect id="SVGID_1_" width="60.7" height="50" />
           </defs>
 
-          <g className="st2">
+          <g>
             <path
-              className="st3"
+              className="fill-primary"
               d="M43.9,13.3c3.2,4.8,11.8,4.2,15,8.9c3.3,4.7,1.3,14.8,0,19.7c-0.9,3.5-4.9,6.3-9.5,7.5L46.1,50
 				c-0.6,0-1.2,0.1-1.8,0c-6.4-0.2-12.2-4.1-19-6s-14.8-1.9-19.6-5.2C0.8,35.5-1,28.9,0.7,23.1c1.7-5.7,4-9.2,9.9-13.6
 				c5.9-4.4,7-5.1,11-8.4c2.7-0.9,5.5-1.4,7.1-0.9l2.9,0.9l3.2,1.7l3.5,2.6l1.4,1.4C42.4,9.4,41.9,10.2,43.9,13.3z"
             />
             <path
-              className="st4"
+              className="fill-dark"
               d="M11.6,25.7c-0.5,0-0.8-0.1-1.3-0.1c-2.4-0.9-3.3-2.4-2.4-4.5c0.9-1.6,2-3.1,3.1-4.5c0.1-0.1,0.2-0.2,0.3-0.3
 				c0.7,0.8,1.4,1.6,2,2.5c0.1,0.1,0.2,0.2,0.4,0.3c2.4-0.9,4.8-1.6,7.3-2.1c2.2-0.4,4.4-0.5,6.7-0.3c0.1-0.3,0.2-0.6,0.5-0.9
 				c0.9-0.7,1.8-0.7,2.7,0.1c0.7-0.6,1.4-0.8,2.3-0.4c0.8,0.7,1,1.4,0.7,2.4c-0.7,1.1-1.7,1.9-3,2.4c-0.5-0.3-1.1-0.6-1.6-0.9
@@ -41,12 +41,12 @@ const index = () => {
 				c-0.4-0.5-0.8-0.9-1.1-1.4c-1.2-1.3-2.7-2.2-4.4-2.7c-0.3,0.4-0.6,0.9-1,1.3C13.4,25.2,12.5,25.6,11.6,25.7z"
             />
             <path
-              className="st5"
+              className="fill-white"
               d="M10.5,19.8c0.5,0,1.1,0,1.7,0c0,1.3,0,2.6,0,3.9c-0.4,0-0.8,0-1.2,0c0-1,0-1.9,0-2.9c-0.2,0-0.3,0-0.5,0
 				C10.5,20.4,10.5,20.1,10.5,19.8z"
             />
             <path
-              className="st6"
+              className="fill-white"
               d="M33.5,26.1c0.8-0.1,1.3,0.2,1.5,0.9c0,0.2-0.2,0.5-0.4,0.6c0.5,0.5,0.5,1,0,1.5c-0.6,0.3-1.2,0.3-1.7-0.1
 				c-0.2-0.3-0.2-0.5-0.2-0.7c0.5,0,0.8,0,1.1,0.3c0.5-0.4,0.4-0.6-0.3-0.6c0-0.2,0-0.5,0-0.7c0.5,0,0.6-0.1,0.4-0.5
 				c-0.2,0-0.2,0.1-0.2,0.2c-0.3,0-0.7,0-1,0C32.7,26.6,32.9,26.3,33.5,26.1z"
@@ -54,19 +54,34 @@ const index = () => {
           </g>
         </g>
       </g>
-      <text transform="matrix(0.7588 0 0 1 39.334 27)" className="st8 st9">
+      <text
+        transform="matrix(0.7588 0 0 1 39.334 27)"
+        className="text-xs fill-white"
+      >
         <FormattedMessage defaultMessage="Could saves up" />
       </text>
-      <text transform="matrix(0.7588 0 0 1 39.334 30.6006)" className="st8 st9">
+      <text
+        transform="matrix(0.7588 0 0 1 39.334 30.6006)"
+        className="text-xs fill-white"
+      >
         <FormattedMessage defaultMessage="to three lives" />
       </text>
-      <text transform="matrix(0.7588 0 0 1 17.2993 11.04)" className="st8 st9">
+      <text
+        transform="matrix(0.7588 0 0 1 17.2993 11.04)"
+        className="text-xs fill-white"
+      >
         <FormattedMessage defaultMessage="Did you know ?" />
       </text>
-      <text transform="matrix(0.7588 0 0 1 4.8252 30.3604)" className="st8 st9">
+      <text
+        transform="matrix(0.7588 0 0 1 4.8252 30.3604)"
+        className="text-xs fill-white"
+      >
         <FormattedMessage defaultMessage="One blood" />
       </text>
-      <text transform="matrix(0.7588 0 0 1 4.8252 33.96)" className="st8 st9">
+      <text
+        transform="matrix(0.7588 0 0 1 4.8252 33.96)"
+        className="text-xs fill-white"
+      >
         <FormattedMessage defaultMessage="donation" />
       </text>
     </svg>
