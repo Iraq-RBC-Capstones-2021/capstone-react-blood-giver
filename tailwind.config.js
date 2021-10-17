d783c9bdfc373517b31fcb23ee494ee4a74c6120
marwa <marwajawad19@gmail.com>
2021-10-17 16:13:01 +0300

    adding fill and font size value to the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontSize: {
+      xs: "3px",
+    },
+    fill: {
+      primary: "#ff867f",
+      white: "white",
+      dark: "#C50E29",
+    },
+
     extend: {},
   },
+
   variants: {
     extend: {},
   },
