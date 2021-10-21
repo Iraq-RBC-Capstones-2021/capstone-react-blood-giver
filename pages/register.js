e7d604a3de66d07c84f38cfee9f4f557c53612ee
marwa <marwajawad19@gmail.com>
2021-10-22 02:57:00 +0300

    passing create donor dispatch ,handle success and failure post request using toast message

--- pages/register.js
+++ pages/register.js
@@ -7,16 +7,40 @@ import { useState } from "react";
 import { useFormik } from "formik";
 import RegisterContext from "../components/RegisterSteps/StateContext";
 import registerSchema from "../components/RegisterSteps/validationSchema";
+import { useDispatch, useSelector } from "react-redux";
+import { createDonor } from "../store/donors";
+import { useToast } from "@chakra-ui/react";
+import uuid from "react-uuid";
 
 export default function Register() {
   const [eligState, setEligState] = useState({
     isProperWeight: false,
     isProperAge: false,
   });
-
   const { step, goToNextStep, backToPrevStep, CurrentStepComponent } =
     useStepper(registerSteps);
-
+  const distpatch = useDispatch();
+  const toast = useToast();
+  const onSuccess = () => {
+    return toast({
+      title: "Request sent",
+      description: "We've register you as donor !",
+      status: "success",
+      position: "top",
+      duration: 9000,
+      isClosable: true,
+    });
+  };
+  const onFailure = () => {
+    return toast({
+      title: "Request failed !",
+      description: "something wrong happened !",
+      status: "failure",
+      position: "top",
+      duration: 9000,
+      isClosable: true,
+    });
+  };
   const {
     values,
     errors,
@@ -32,11 +56,13 @@ export default function Register() {
       age: "",
       city: "",
       bloodType: "",
+      id: uuid(),
     },
     validationSchema: registerSchema,
     validateOnBlur: true,
     onSubmit: (values) => {
       goToNextStep && goToNextStep();
+      distpatch(createDonor({ values, onSuccess, onFailure }));
     },
   });
 
