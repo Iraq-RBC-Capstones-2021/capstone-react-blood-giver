70e1336fbd1c0c40433d7b4a0f57d9b666cde70e
marwa <marwajawad19@gmail.com>
2021-11-03 22:25:23 +0300

    fix button space issue

--- pages/Donors/index.js
+++ pages/Donors/index.js
@@ -2,7 +2,7 @@ import { Container, Box, Text } from "@chakra-ui/react";
 import { FormattedMessage } from "react-intl";
 import { wrapper } from "../store";
 import { fetchDonors } from "../store/donors/donorSlice";
-import ContainerDonors from "../components/donor-content/Container";
+import ContainerDonors from "../../components/donor-content/Container";
 function index() {
   return (
     <Container maxW="container.xl">
