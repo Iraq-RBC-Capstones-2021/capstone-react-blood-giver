37b3acafa895d8beef6006a0a7be6eee9329529f
ahmad <ahmad.00079821@gmail.com>
2021-11-04 15:55:36 +0300

    Fix URL Fixes #43

--- store/donors/donorSlice.js
+++ store/donors/donorSlice.js
@@ -5,7 +5,7 @@ export const fetchDonors = createAsyncThunk(
   "donorStore/fetchDonorData",
   async () => {
     return await axios
-      .get("http://localhost:3000/api/donors")
+      .get("https://capstone-react-blood-giver.vercel.app/api/donors")
       .then((response) => response.data);
   }
 );
